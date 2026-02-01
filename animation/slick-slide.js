6b1a159c3b33754a5141dc57dcb59399b091794e
Rishimohan Yadav <mr.rishimohan.01@gmail.com>
2026-02-01 13:33:05 +0530

    update all the navigation links and about pages & add wow slider

--- animation/slick-slide.js
+++ animation/slick-slide.js
@@ -11,3 +11,33 @@ $(document).ready(function () {
         adaptiveHeight: true
     });
 });
+
+
+// vision image slider
+$(document).ready(function () {
+  $('.vision-img-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    infinite: true,
+    arrows: true,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: { slidesToShow: 3 }
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 2 }
+      },
+      {
+        breakpoint: 576,
+        settings: { slidesToShow: 1 }
+      }
+    ]
+  });
+});
+
